8067ebc6df541111e51e81a6b3e2f218294f470d
Rob <robtennisvandenberg@gmail.com>
2015-06-10 14:21:43 +0200

    Updated vibration

--- app/assets/scripts/vibration.js
+++ app/assets/scripts/vibration.js
@@ -1,11 +1,10 @@
 // enable vibration support
 navigator.vibrate = navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate || navigator.msVibrate;
- 
-if (navigator.vibrate) {
-    // vibration API supported
-}
 
 function vibrate(vibrateTime)
 {
-	navigator.vibrate(vibrateTime);
+	if (navigator.vibrate) {
+		// vibration API supported
+		navigator.vibrate(vibrateTime);
+	}
 }
