a03d4e9a70da1cf1f7c8c6a218240b1cd4dcb8aa
Rob <robtennisvandenberg@gmail.com>
2015-06-12 12:53:36 +0200

    Reformatted code

--- app/assets/scripts/vibration.js
+++ app/assets/scripts/vibration.js
@@ -1,10 +1,9 @@
 // enable vibration support
 navigator.vibrate = navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate || navigator.msVibrate;
 
-function vibrate(vibrateTime)
-{
-	if (navigator.vibrate) {
-		// vibration API supported
-		navigator.vibrate(vibrateTime);
-	}
+function vibrate(vibrateTime) {
+    if (navigator.vibrate) {
+        // vibration API supported
+        navigator.vibrate(vibrateTime);
+    }
 }
