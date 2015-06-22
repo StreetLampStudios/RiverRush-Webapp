f5b3f7d89f44ea3c6f37edaf4dc38ab8eb65f305
Rob <robtennisvandenberg@gmail.com>
2015-06-22 15:02:09 +0200

    Testing if these kind of tests work

--- test/spec/test_test.js
+++ test/spec/test_test.js
@@ -3,3 +3,17 @@ describe("A suite", function() {
     expect(true).toBe(true);
   });
 });
+
+describe("Testing testing", function() {
+	it("can do calls to my code, right?", function () {
+		calculateWaveSpot(0);
+		expect(wave_x).toBe(0);
+		expect(wave_y).toBe(0);
+	}
+	
+	it("will work for this too, won't it?", function () {
+		calculateWaveSpot(100);
+		expect(wave_x).not.toBe(0);
+		expect(wave_y).not.toBe(0);
+	}
+});
