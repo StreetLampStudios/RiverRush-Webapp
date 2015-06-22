27e347893b26fd13637fdbc716c502d71d13954a
Rob <robtennisvandenberg@gmail.com>
2015-06-22 16:28:54 +0200

    Testing actual functionality

--- test/spec/test_test.js
+++ test/spec/test_test.js
@@ -1,20 +1,19 @@
 describe("A suite", function() {
   it("contains spec with an expectation", function() {
-	AnimalJumpedEvent();
     expect(true).toBe(true);
   });
 });
 
-/*describe("Testing testing", function() {
+describe("Testing testing", function() {
 	it("can do calls to my code, right?", function () {
 		calculateWaveSpot(0);
 		expect(wave_x).toBe(0);
 		expect(wave_y).toBe(0);
-	}
+	});
 	
 	it("will work for this too, won't it?", function () {
 		calculateWaveSpot(100);
 		expect(wave_x).not.toBe(0);
 		expect(wave_y).not.toBe(0);
-	}
-});*/
+	});
+});
