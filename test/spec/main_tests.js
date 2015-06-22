dccece8880b3d1566e6cb5d9ef3435c783053c56
Rob <robtennisvandenberg@gmail.com>
2015-06-22 20:16:23 +0200

    Adding some tests for the main (I included a test that should fail on purpose to see if the build will fail)

---
+++ test/spec/main_tests.js
@@ -0,0 +1,17 @@
+describe("Main Testing", function() {
+	it("should turn on the overlay correctly", function () {
+		expect(overlayvisible).toBe(false);
+		turnOnOverlay();
+		expect(overlayvisible).toBe(true);
+		turnOnOverlay();
+		expect(overlayvisible).toBe(false);
+	});
+	
+	it("should turn off the overlay correctly", function () {
+		expect(overlayvisible).toBe(true);
+		turnOffOverlay();
+		expect(overlayvisible).toBe(false);
+		turnOffOverlay();
+		expect(overlayvisible).toBe(false);
+	});
+});
