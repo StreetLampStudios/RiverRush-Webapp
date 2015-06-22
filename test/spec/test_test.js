e990c5925b4b13708c0cf58873df8b9902d1b32d
Rob <robtennisvandenberg@gmail.com>
2015-06-22 21:49:36 +0200

    Added some testing in for main

--- test/spec/test_test.js
+++ test/spec/test_test.js
@@ -1,19 +1,10 @@
 describe("A suite", function() {
+	// Just for making sure Jasmine is working
   it("contains spec with an expectation", function() {
     expect(true).toBe(true);
   });
-});
-
-describe("Testing testing", function() {
-	it("can do calls to my code, right?", function () {
-		calculateWaveSpot(0);
-		expect(wave_x).toBe(0);
-		expect(wave_y).toBe(0);
-	});
-	
-	it("will work for this too, won't it?", function () {
-		calculateWaveSpot(100);
-		expect(wave_x).not.toBe(0);
-		expect(wave_y).not.toBe(0);
-	});
+  
+  it("Another spec", function() {
+    expect(true).not.toBe(false);
+  });
 });
